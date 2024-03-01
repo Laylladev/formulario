0aa92f1985b0db4a938a2293961d3d30da14606a
Laylladev <layllacris648@gmail.com>
2024-03-01 15:25:47 -0400

    coreçaoformulario

--- main.js
+++ main.js
@@ -1,10 +1,31 @@
-function logar(){
-    const login = document.getElementById('login').value;
-    const senha = document.getElementById('senha').value;
+const form = document.getElementById('form-confirm');
 
-    if(senha =="admin"){
-        alert('sucesso');
-    } else{
-        alert('Usuario ou senha incorretos');
-    }
+let formValid = false;
+
+function numbervalid (ValA, ValB){
+    return ValB > ValA;
 }
+
+form.addEventListener('submit', function(e){
+    e.preventDefault(); //previne carregamento da página após "submit"
+    let ValA = parseFloat(document.getElementById('valor-a').value);
+    let ValB = parseFloat(document.getElementById('valor-b').value);
+
+    const messagesucess = 'formulario enviado com sucesso! Verificando: <b> ValorA menor que ValorB!</B>';
+    const containermessagesucess = document.querySelector('.success-message');
+    
+    formValid = numbervalid(ValA, ValB);
+    
+    if (formValid){
+        document.querySelector('.error-message').style.display = 'none';
+        containermessagesucess.innerHTML = messagesucess;
+        containermessagesucess.style.display = 'block';
+        document.querySelector('.error-message').style.display = 'none';
+
+        ValA.value = '';
+        ValB.value = '';
+    } else {
+        document.querySelector('.error-message').style.display = 'block';
+        containermessagesucess.style.display = 'none';
+    }    
+})
